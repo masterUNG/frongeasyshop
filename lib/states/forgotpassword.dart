4725a3887c8dd84d4d50430fdc015661ae7a4575
Phakatip <pkatip1826@gmail.com>
2021-12-12 10:57:47 +0700

    update 12 Dec

--- lib/states/forgotpassword.dart
+++ lib/states/forgotpassword.dart
@@ -1,4 +1,4 @@
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:frongeasyshop/utility/my_constant.dart';
 
@@ -10,16 +10,42 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
-  //String email;
- // final auth = FirebaseAuth.instance;
+  late String email;
+  final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: MyConstant.primart,
-        title: const Text('ลืมรหัสผ่าน'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: MyConstant.primart,
+          title: const Text('ลืมรหัสผ่าน'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: ('Email ผู้ใช้งาน'),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              RaisedButton(
+                child: Text('รีเซ็ตรหัสผ่าน'),
+                onPressed: () {
+                  auth.sendPasswordResetEmail(email: email);
+                  Navigator.of(context).pop();
+                },
+              )
+            ])
+          ],
+        ));
   }
 }
